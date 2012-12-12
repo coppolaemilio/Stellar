a82f3a1f8a34cde5e6c90aa9eaf16b28ed6dee26
Ado Matejov <a.matejov@centrum.sk>
2012-12-12 21:47:20 +0100

    uncommented

--- cfg.py
+++ cfg.py
@@ -18,10 +18,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Stellar.  If not, see <http://www.gnu.org/licenses/>.
 
-#from __future__ import division
-#from __future__ import absolute_import
-#from __future__ import print_function
-#from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
 import ConfigParser, codecs
