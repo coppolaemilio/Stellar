c412f51a6a27aefce5340a5d33318d8b15c2af68
Emilio Coppola <coppolaemilio@gmail.com>
2012-12-31 15:43:53 +0100

    ObjectGUI updated with Event preview

--- src/objectgui.py
+++ src/objectgui.py
@@ -48,78 +48,56 @@ class ObjectGUI(QtGui.QWidget):
         self.ContainerGrid = QtGui.QGridLayout(self.main)
         self.ContainerGrid.setMargin (0)
 
+        
         self.LblName = QtGui.QLabel('Name:')
         self.nameEdit = QtGui.QLineEdit(self.FileName)
-
         self.BnewSprite = QtGui.QPushButton("New")
         self.BeditSprite = QtGui.QPushButton("Edit")
         self.SpriteCombo = QtGui.QComboBox()
         self.SpriteCombo.addItem('<no sprite>')
         self.SpriteCombo.addItems(self.tree.main.Sources["Sprites"])
-        
         self.SpriteFrame = QtGui.QGroupBox("Sprite")
-
+        
         self.spritelayout = QtGui.QGridLayout()
-
         self.spritelayout.setMargin (8)
-
         self.spritelayout.addWidget(self.SpriteCombo,0,0,1,2)
-
         self.spritelayout.addWidget(self.BnewSprite,1,0)
         self.spritelayout.addWidget(self.BeditSprite,1,1)
-
         self.SpriteFrame.setLayout(self.spritelayout)
 
+        
         self.NameFrame = QtGui.QFrame()
-
         self.namelayout = QtGui.QGridLayout()
-
         self.namelayout.setMargin (0)
-
         self.namelayout.addWidget(self.LblName,0,0)
-
         self.namelayout.addWidget(self.nameEdit,0,1)
-
         self.namelayout.addWidget(self.SpriteFrame,1,0,1,2)
-
         self.NameFrame.setLayout(self.namelayout)
 
+        
         self.cbvisible = QtGui.QCheckBox('Visible', self)
-
         self.cbsolid = QtGui.QCheckBox('Solid', self)
-
         self.cbpersis = QtGui.QCheckBox('Persistent', self)
-
         self.LblDepth = QtGui.QLabel('Depth:')
-
         self.depthEdit = QtGui.QLineEdit("0")
-
         self.LblParent = QtGui.QPushButton("Parent:")
-
         self.ParentEdit = QtGui.QComboBox()
-
         self.LblMask = QtGui.QPushButton("Mask:")
-
         self.MaskEdit = QtGui.QComboBox()
-
         self.Btninfo = QtGui.QPushButton("Show Information")
-
         self.Btninfo.setIcon(QtGui.QIcon(os.path.join('Data', 'info.png')))
-
         self.Btnok = QtGui.QPushButton("OK")
-
         self.Btnok.setIcon(QtGui.QIcon('Data/accept.png'))
-
         self.eventstree = QtGui.QTreeWidget()
         self.eventstree.setHeaderLabel("Events")
-
+        events = ["Create", "Destroy"]
+        create = QtGui.QTreeWidgetItem(self.eventstree,QtCore.QStringList(events[0]))
+        create.setIcon(0, QtGui.QIcon(os.path.join('Data', 'Events', 'create.png')))
+        
         self.actionstree = QtGui.QTreeWidget()
         self.actionstree.setHeaderLabel("Actions")
-
         self.Btnaddevent = QtGui.QPushButton("Add Event")
-
         self.Btndelete = QtGui.QPushButton("Delete")
-
         self.Btnchange = QtGui.QPushButton("Change")
 
         
@@ -129,70 +107,48 @@ class ObjectGUI(QtGui.QWidget):
 
         self.FirstWidget = QtGui.QWidget()
         self.FirstGrid = QtGui.QGridLayout()
-
         self.FirstGrid.addWidget(self.NameFrame, 0, 0, 1, 2)
-
         self.FirstGrid.addWidget(self.SpriteFrame, 1, 0, 1, 2)
-
         self.FirstGrid.addWidget(self.cbvisible, 2, 0)
-
         self.FirstGrid.addWidget(self.cbsolid, 2, 1)
-
         self.FirstGrid.addWidget(self.cbpersis, 3, 0)
-
         self.FirstGrid.addWidget(self.LblDepth, 4, 0)
-
         self.FirstGrid.addWidget(self.depthEdit, 4, 1)
-
         self.FirstGrid.addWidget(self.LblParent, 5, 0)
-
         self.FirstGrid.addWidget(self.ParentEdit, 5, 1)
-
         self.FirstGrid.addWidget(self.LblMask, 6, 0)
-
         self.FirstGrid.addWidget(self.MaskEdit, 6, 1)
-
         self.FirstGrid.addItem(spacerItem)
-
         self.FirstGrid.addWidget(self.Btninfo, 7, 0, 1, 2)
-
         self.FirstGrid.addWidget(self.Btnok, 8, 0,1,2)
         self.FirstWidget.setLayout(self.FirstGrid)
         
 
         #---
-
         self.SecondWidget = QtGui.QWidget()
         self.SecondGrid = QtGui.QGridLayout()
-
         self.SecondGrid.addWidget(self.eventstree, 0, 0, 7, 2)
-
         self.SecondGrid.addWidget(self.Btnaddevent,    7, 0, 1, 2)
-
         self.SecondGrid.addWidget(self.Btndelete,      8, 0)
-
         self.SecondGrid.addWidget(self.Btnchange,      8, 1)
         self.SecondWidget.setLayout(self.SecondGrid)
 
         #---
         self.ThirdWidget = QtGui.QWidget()
         self.ThirdGrid = QtGui.QGridLayout()
-
         self.ThirdGrid.addWidget(self.actionstree, 0, 0, 9, 2)
         self.ThirdWidget.setLayout(self.ThirdGrid)
 
 
         self.objectsplitter = QtGui.QSplitter(QtCore.Qt.Horizontal, self)
-
         self.objectsplitter.addWidget(self.FirstWidget)
-
         self.objectsplitter.addWidget(self.SecondWidget)
         self.objectsplitter.addWidget(self.ThirdWidget)
         self.objectsplitter.setStretchFactor(1, 1)
         
 
         self.ContainerGrid.addWidget(self.objectsplitter)
-        
+
 
 
 
