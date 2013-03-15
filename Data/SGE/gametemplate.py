8da5b601378d7302cd678cfc254270247535b2ca
Emilio Coppola <coppolaemilio@gmail.com>
2013-03-16 04:45:59 +0700

    Game template
    The game template is the file that Stellar uses for creating the final
    game based on the resources of the projects.

---
+++ Data/SGE/gametemplate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sge
+import os
+#Imports
+
+
+#Paths
+sge.image_directories = [os.path.join('Sprites'),os.path.join('Backgrounds')]
+sge.font_directories = [os.path.join('Fonts')]
+sge.sound_directories = [os.path.join('Sound')]
+sge.music_directories = [os.path.join('Sound')]
+
+
+#Game Class
+class Game(sge.Game):
+    def event_key_press(self, key):
+        if key == 'escape':
+            self.end()
+
+    def event_close(self):
+        self.end()
+        
+# Add Stellar objects
+
+
+
+game = Game()
+# Load sprites
+
+# Rooms
+
+
+game.start()
+
