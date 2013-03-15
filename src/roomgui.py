94f319c63b21153f7de53f58edbeceb52d683b17
Emilio Coppola <coppolaemilio@gmail.com>
2013-03-16 04:45:16 +0700

    RoomGUI
    Provitional text editor for creating SGE rooms.
    The text editor will be replaced so everything can be made on a visual
    way.

--- src/roomgui.py
+++ src/roomgui.py
@@ -16,51 +16,160 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
+# along with Stellar.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt4.Qt import Qt
-from PyQt4 import QtGui, QtCore
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
+
+import sys
 import os
+import shutil
+
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.Qsci import QsciScintilla, QsciLexerPython
+
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read('config.ini')
+
+fontconfig = config.get('colors', 'font').decode('utf-8')
+sizeconfig = config.get('colors', 'size').decode('utf-8')
+backgroundcolor = config.get('colors', 'background').decode('utf-8')
+actualline = config.get('colors', 'actualline').decode('utf-8')
+fontcolor = config.get('colors', 'fontcolor').decode('utf-8')
+commentcol = config.get('colors', 'comment').decode('utf-8')
+numbercol = config.get('colors', 'number').decode('utf-8')
 
 
-class RoomGUI(QtGui.QWidget):
-    def __init__(self, main, dirname):
-        super(RoomGUI, self).__init__()
-        self.main = main
-        self.dirname = dirname
-        self.initUI()
+class SimplePythonEditor(QsciScintilla):
+    ARROW_MARKER_NUM = 8
+
+
+    def __init__(self, parent=None):
+        super(SimplePythonEditor, self).__init__(parent)
         
+        # Set the default font
+        font = QFont()
+        font.setFamily(fontconfig)
+        font.setFixedPitch(False)
+        font.setPointSize(int(sizeconfig))
+        self.setFont(font)
+        self.setMarginsFont(font)
+        
+
+        # Margin 0 is used for line numbers
+        fontmetrics = QFontMetrics(font)
+        self.setMarginsFont(font)
+        self.setMarginWidth(0, 30)
+        self.setMarginLineNumbers(0, True)
+        self.setMarginsBackgroundColor(QColor("#cccccc"))
+
+        # Clickable margin 1 for showing markers
+        self.setMarginSensitivity(1, True)
+        self.connect(self,
+            SIGNAL('marginClicked(int, int, Qt::KeyboardModifiers)'),
+            self.on_margin_clicked)
+        self.markerDefine(QsciScintilla.RightArrow,
+            self.ARROW_MARKER_NUM)
+        self.setMarkerBackgroundColor(QColor("#ee1111"),
+            self.ARROW_MARKER_NUM)
+
+        # Brace matching: enable for a brace immediately before or after
+        # the current position
+        #
+        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+        
+        #replacing tabs for 4 spaces
+        self.setIndentationWidth(4)
+        
+        #set autocomplete
+        self.autoCompleteFromAPIs()
+
+        # Current line visible with special background color
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QColor(str(actualline)))
+
+        # Set Python lexer
+        # Set style for Python comments (style number 1) to a fixed-width
+        # courier.
+        #
+        lexer = QsciLexerPython()
+        lexer.setDefaultFont(font)
+        lexer.setDefaultColor(QColor(str(fontcolor)))
+        lexer.setDefaultPaper(QColor(str(backgroundcolor)))
+        self.setLexer(lexer)
+        """  Default = 0, Comment = 1, Number = 2, 
+        DoubleQuotedString = 3, SingleQuotedString = 4, Keyword = 5, 
+        TripleSingleQuotedString = 6, TripleDoubleQuotedString = 7, ClassName = 8, 
+        FunctionMethodName = 9, Operator = 10, Identifier = 11, 
+        CommentBlock = 12, UnclosedString = 13, HighlightedIdentifier = 14, 
+        Decorator = 15 """
+        self.SendScintilla(QsciScintilla.SCI_STYLESETFORE, 1, QColor(str(commentcol)))
+        self.SendScintilla(QsciScintilla.SCI_STYLESETFORE, 2, QColor(str(numbercol)))
+
+        # Don't want to see the horizontal scrollbar at all
+        # Use raw message to Scintilla here (all messages are documented
+        # here: http://www.scintilla.org/ScintillaDoc.html)
+        #self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+
+        # not too small
+        #self.setMinimumSize(600, 450)
+
+    def on_margin_clicked(self, nmargin, nline, modifiers):
+        # Toggle marker for the line the margin was clicked on
+        if self.markersAtLine(nline) != 0:
+            self.markerDelete(nline, self.ARROW_MARKER_NUM)
+        else:
+            self.markerAdd(nline, self.ARROW_MARKER_NUM)
+            
+class RoomGUI(QtGui.QWidget):
+  
+    def __init__(self, main, FileName, dirname, tree):
+        super(RoomGUI, self).__init__()
+        
+        self.main = main
+        self.tree = tree
+        self.dirname = dirname
+        self.FileName = FileName
+        self.initUI()
 
     def initUI(self):
         self.ContainerGrid = QtGui.QGridLayout(self)
         self.ContainerGrid.setMargin (0)
-
-        #TOOLBAR
+        
+        editor = self.textEdit = SimplePythonEditor()
+        
         self.LblName = QtGui.QLabel('Name:')
-        self.nameEdit = QtGui.QLineEdit()
-        #self.nameEdit.textChanged[str].connect(self.onChanged)
+        self.nameEdit = QtGui.QLineEdit(self.FileName)
+        self.nameEdit.textChanged[str].connect(self.onChanged)
 		
         saveAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'tick.png')), 'Save', self)
         saveAction.setShortcut('Ctrl+S')
-        #saveAction.triggered.connect(self.saveScript)
+        saveAction.triggered.connect(self.saveScript)
 		
         exportAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'save.png')), 'Export', self)
-        #exportAction.triggered.connect(self.exportScript)
+        exportAction.triggered.connect(self.exportScript)
 		
         importAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'folder.png')), 'Open', self)
-        #importAction.triggered.connect(self.openScript)
+        importAction.triggered.connect(self.openScript)
 
         self.undoAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'undo.png')), 'Undo', self)
-        #self.undoAction.triggered.connect(self.undo)
+        self.undoAction.triggered.connect(self.undo)
         
         self.redoAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'redo.png')), 'Redo', self)
-        #self.redoAction.triggered.connect(self.redo)
+        self.redoAction.triggered.connect(self.redo)
         
         self.whitespacevisAction = QtGui.QAction(QtGui.QIcon(os.path.join('Data', 'font.png')), 'White Space', self)
-        #self.whitespacevisAction.triggered.connect(self.whitespace)
+        self.whitespacevisAction.triggered.connect(self.whitespace)
         self.visible= False
 		
-        self.toolbar = QtGui.QToolBar('Script Toolbar')
+        self.toolbar = QtGui.QToolBar('Rooms Toolbar')
         self.toolbar.setIconSize(QtCore.QSize(16, 16))
         self.toolbar.addAction(saveAction)
         self.toolbar.addSeparator()
@@ -74,21 +183,79 @@ class RoomGUI(QtGui.QWidget):
         self.toolbar.addSeparator()
         self.toolbar.addWidget(self.LblName)
         self.toolbar.addWidget(self.nameEdit)
-        #/TOOLBAR
 
+
+        self.ContainerGrid.setSpacing(0)
+        self.ContainerGrid.addWidget(editor, 1, 0,1,15)
+        self.ContainerGrid.addWidget(self.toolbar, 0, 0)
+		
+        self.startopen()
         
-        self.FirstWidget = QtGui.QTextEdit()
-        self.scrollArea = QtGui.QScrollArea()
-        self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
-
-        self.splitterwidg = QtGui.QWidget()
-        self.objectsplitter = QtGui.QHBoxLayout()
-        self.objectsplitter.setMargin (0)
-        self.objectsplitter.addWidget(self.FirstWidget)
-        self.objectsplitter.addWidget(self.scrollArea)
-        self.splitterwidg.setLayout(self.objectsplitter)
-
+        self.main.setWindowTitle("Rooms Properties: "+ self.FileName)
         
-        self.ContainerGrid.addWidget(self.toolbar)
-        self.ContainerGrid.addWidget(self.splitterwidg)
         self.setLayout(self.ContainerGrid)
+        
+        self.show()
+        
+     
+
+    def onChanged(self, text):
+        self.main.setWindowTitle("Rooms Properties: "+ text)
+        self.main.setWindowIcon(QtGui.QIcon(os.path.join('Data', 'addscript.png')))
+        fname = self.FileName + ".py"
+        finalname = str(text) + ".py"
+
+        #rename file in folder
+        os.rename(os.path.join(self.dirname, "Rooms", str(self.FileName)) + ".py",
+                  os.path.join(self.dirname, "Rooms", finalname))
+        
+        #rename file in tree widget
+        self.main.updatetree()
+        self.FileName = text
+
+    def whitespace(self):
+        self.visible^= True
+        self.textEdit.setWhitespaceVisibility(self.visible)
+
+    def undo(self):
+        self.textEdit.undo()
+        
+    def redo(self):
+        self.textEdit.redo()
+	
+    def exportScript(self):
+        #print str(self.dirname)+ ("/Scripts/") + str(self.FileName)+".py"
+        fname = os.path.join(self.dirname, "Rooms", str(self.FileName))+ ".py"
+        with open(fname, 'w') as f:    
+            data = self.textEdit.text()
+            f.write(data)
+            f.close()
+
+    def saveScript(self):
+        #print str(self.dirname)+ ("/Scripts/") + str(self.FileName)+".py"
+        fname = os.path.join(self.dirname, "Rooms", str(self.FileName)) + ".py"
+        with open(fname, 'w') as f:    
+            data = self.textEdit.text()
+            f.write(data)
+            f.close()
+        self.main.qmdiarea.activeSubWindow().close()
+			
+    def startopen(self):
+        fname = os.path.join(self.dirname, "Rooms", str(self.FileName)) + ".py"
+
+        with open(fname, 'r') as f:
+            data = f.read()
+            self.textEdit.setText(data)
+            f.close()
+			
+    def openScript(self):
+
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+                str(os.getcwd()))
+
+        if fname == '':
+            return
+
+        with open(fname, 'r') as f:
+            data = f.read()
+            self.textEdit.setText(data)
